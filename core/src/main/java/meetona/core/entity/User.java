9ce6a5bdec123055856dc6f6b7e2de84aa1a7729
Emmy Steven <hello@emmysteven.com>
2023-09-01 01:23:08 +0100

    refactor: implement UserDetails

--- core/src/main/java/meetona/core/entity/User.java
+++ core/src/main/java/meetona/core/entity/User.java
@@ -5,9 +5,14 @@ import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.data.annotation.Transient;
 import meetona.core.common.BaseEntity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 
 @Data
@@ -17,7 +22,7 @@ import java.util.Set;
 @AllArgsConstructor
 @Table(name = "users")
 @EqualsAndHashCode(callSuper = false)
-public class User extends BaseEntity {
+public class User extends BaseEntity implements UserDetails {
 
     private String firstname;
     private String lastname;
@@ -36,5 +41,36 @@ public class User extends BaseEntity {
     private Units units;
 
     private Boolean isEmailVerified;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles.stream()
+                .map(role -> new SimpleGrantedAuthority(role.name()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() { return username;}
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() { return true; }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() { return true; }
 
 }
