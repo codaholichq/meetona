b9003639a8a058af1d7e7ea0a68c1659d218524c
Emmy Steven <hello@emmysteven.com>
2023-09-01 02:22:30 +0100

    optimise imports

--- data/src/main/java/meetona/data/config/SecurityConfig.java
+++ data/src/main/java/meetona/data/config/SecurityConfig.java
@@ -21,8 +21,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import java.util.Optional;
-
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
