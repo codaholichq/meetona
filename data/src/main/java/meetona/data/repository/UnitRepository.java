6c3f12667d9dc3629e9b576fca6054ba0f3f78eb
Emmy Steven <hello@emmysteven.com>
2023-09-02 17:35:01 +0100

    add repository for Unit entity

---
+++ data/src/main/java/meetona/data/repository/UnitRepository.java
@@ -0,0 +1,11 @@
+package meetona.data.repository;
+
+import meetona.core.entity.Unit;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface UnitRepository extends JpaRepository<Unit, UUID> {
+    Boolean existsByName(String name);
+    Boolean existsByLocation(String location);
+}
