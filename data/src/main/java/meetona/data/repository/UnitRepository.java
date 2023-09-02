5b9cadccb517b69de762d981c62f51969ecfac46
Emmy Steven <hello@emmysteven.com>
2023-09-02 23:46:55 +0100

    fix: replace UnitDto with Unit entity

--- data/src/main/java/meetona/data/repository/UnitRepository.java
+++ data/src/main/java/meetona/data/repository/UnitRepository.java
@@ -1,11 +1,14 @@
 package meetona.data.repository;
 
+import meetona.core.Dto.response.UnitDto;
 import meetona.core.entity.Unit;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
 public interface UnitRepository extends JpaRepository<Unit, UUID> {
+    Unit getById(UUID id);
+    UnitDto getAll();
     Boolean existsByName(String name);
     Boolean existsByLocation(String location);
 }
