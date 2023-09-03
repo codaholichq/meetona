36154089f6c98f965c19b54315ef3c7836765af7
Emmy Steven <hello@emmysteven.com>
2023-09-04 00:41:09 +0100

    remove redundant methods
    fix #4

--- data/src/main/java/meetona/data/repository/UnitRepository.java
+++ data/src/main/java/meetona/data/repository/UnitRepository.java
@@ -1,14 +1,10 @@
 package meetona.data.repository;
 
-import meetona.core.Dto.response.UnitDto;
 import meetona.core.entity.Unit;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
 public interface UnitRepository extends JpaRepository<Unit, UUID> {
-    Unit getById(UUID id);
-    UnitDto getAll();
     Boolean existsByName(String name);
-    Boolean existsByLocation(String location);
 }
