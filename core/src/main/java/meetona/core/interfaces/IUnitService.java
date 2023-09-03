6a15122fd6ae2d3a506333666e69d19d8265d39b
Emmy Steven <hello@emmysteven.com>
2023-09-04 00:39:14 +0100

    add contracts for unit service

--- core/src/main/java/meetona/core/interfaces/IUnitService.java
+++ core/src/main/java/meetona/core/interfaces/IUnitService.java
@@ -4,11 +4,12 @@ import meetona.core.Dto.request.UnitRequest;
 import meetona.core.Dto.response.ApiResponse;
 import meetona.core.Dto.response.UnitDto;
 
+import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 
 public interface IUnitService {
-    CompletableFuture<ApiResponse<UnitDto>> getAll();
+    CompletableFuture<ApiResponse<List<UnitDto>>> getAll();
     CompletableFuture<ApiResponse<UnitDto>> getById(UUID id);
     CompletableFuture<ApiResponse<UnitDto>> add(UnitRequest unitRequest);
     CompletableFuture<ApiResponse<UnitDto>> update(UUID id, UnitRequest unitRequest);
