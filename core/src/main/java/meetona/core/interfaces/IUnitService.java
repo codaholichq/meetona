f15766c0543101aa3dfcdcf89063250e6161f854
Emmy Steven <hello@emmysteven.com>
2023-09-03 16:35:28 +0100

    optimise imports and update method signature

--- core/src/main/java/meetona/core/interfaces/IUnitService.java
+++ core/src/main/java/meetona/core/interfaces/IUnitService.java
@@ -1,7 +1,6 @@
 package meetona.core.interfaces;
 
-import meetona.core.Dto.request.AddUnitDto;
-import meetona.core.Dto.request.UpdateUnitDto;
+import meetona.core.Dto.request.UnitRequest;
 import meetona.core.Dto.response.ApiResponse;
 import meetona.core.Dto.response.UnitDto;
 
@@ -11,7 +10,7 @@ import java.util.concurrent.CompletableFuture;
 public interface IUnitService {
     CompletableFuture<ApiResponse<UnitDto>> getAll();
     CompletableFuture<ApiResponse<UnitDto>> getById(UUID id);
-    CompletableFuture<ApiResponse<UnitDto>> add(AddUnitDto addUnitDto);
-    CompletableFuture<ApiResponse<UnitDto>> update(UUID id, UpdateUnitDto updateUnitDto);
+    CompletableFuture<ApiResponse<UnitDto>> add(UnitRequest unitRequest);
+    CompletableFuture<ApiResponse<UnitDto>> update(UUID id, UnitRequest unitRequest);
     CompletableFuture<ApiResponse<UnitDto>> delete(UUID id);
 }
