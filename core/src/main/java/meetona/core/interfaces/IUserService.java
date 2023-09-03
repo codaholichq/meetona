52df0a1d360a04b883c56631191cc8faa4706ca3
Emmy Steven <hello@emmysteven.com>
2023-09-04 00:39:52 +0100

    update contracts for user service

--- core/src/main/java/meetona/core/interfaces/IUserService.java
+++ core/src/main/java/meetona/core/interfaces/IUserService.java
@@ -4,10 +4,11 @@ import meetona.core.Dto.request.LoginDto;
 import meetona.core.Dto.request.SignupDto;
 import meetona.core.Dto.response.ApiResponse;
 import meetona.core.Dto.response.AuthResponse;
+import meetona.core.Dto.response.UserDto;
 
 import java.util.concurrent.CompletableFuture;
 
 public interface IUserService {
-    CompletableFuture<ApiResponse> register(SignupDto signupDto);
+    CompletableFuture<ApiResponse<UserDto>> register(SignupDto signupDto);
     CompletableFuture<AuthResponse> authenticate(LoginDto loginDto);
 }
