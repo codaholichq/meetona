0642e8b5f12304f5ead3bd5a02b3ced22d82fd83
Emmy Steven <hello@emmysteven.com>
2023-09-04 00:49:43 +0100

    use IoC properly

--- web/src/main/java/meetona/web/controller/UserController.java
+++ web/src/main/java/meetona/web/controller/UserController.java
@@ -1,30 +1,30 @@
 package meetona.web.controller;
 
-import java.util.concurrent.CompletableFuture;
-
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import meetona.core.Dto.request.LoginDto;
+import meetona.core.Dto.request.SignupDto;
+import meetona.core.Dto.response.ApiResponse;
+import meetona.core.Dto.response.AuthResponse;
+import meetona.core.Dto.response.UserDto;
+import meetona.core.interfaces.IUserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import meetona.core.Dto.request.LoginDto;
-import meetona.core.Dto.request.SignupDto;
-import meetona.core.Dto.response.ApiResponse;
-import meetona.core.Dto.response.AuthResponse;
-import meetona.data.service.UserService;
-import lombok.RequiredArgsConstructor;
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/user")
 public class UserController {
 
-    private final UserService userService;
+    private final IUserService userService;
 
     @PostMapping("/register")
-    public CompletableFuture<ResponseEntity<ApiResponse>> register(@Valid @RequestBody SignupDto signupDto) {
+    public CompletableFuture<ResponseEntity<ApiResponse<UserDto>>> register(@Valid @RequestBody SignupDto signupDto) {
         return userService
                 .register(signupDto)
                 .thenApply(ResponseEntity::ok);
