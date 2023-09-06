cbb066521e19c3c211de6a28e2949c52b5fd61fc
Emmy Steven <hello@emmysteven.com>
2023-09-06 01:03:49 +0100

    ignore the users object

--- data/src/main/java/meetona/data/mapper/UnitMapper.java
+++ data/src/main/java/meetona/data/mapper/UnitMapper.java
@@ -3,6 +3,7 @@ package meetona.data.mapper;
 import meetona.core.Dto.response.UnitDto;
 import meetona.core.entity.Unit;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
@@ -11,5 +12,6 @@ public interface UnitMapper {
 
     UnitDto ToUnitDto(Unit unit);
 
+    @Mapping(target = "users", ignore = true)
     Unit ToUnit(UnitDto unitDto);
 }
