2905c98501c8ee2bbc284711603b6f741efc89bd
Emmy Steven <hello@emmysteven.com>
2023-09-08 11:05:11 +0100

    use a simple authentication entry point implementation

--- data/src/main/java/meetona/data/security/AuthEntryPoint.java
+++ data/src/main/java/meetona/data/security/AuthEntryPoint.java
@@ -3,16 +3,11 @@ package meetona.data.security;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
+import meetona.core.util.JsonUtils;
 import org.springframework.http.MediaType;
-import org.springframework.security.authentication.AccountExpiredException;
-import org.springframework.security.authentication.CredentialsExpiredException;
-import org.springframework.security.authentication.DisabledException;
-import org.springframework.security.authentication.LockedException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
-import meetona.core.util.JsonUtils;
 
 import java.io.IOException;
 import java.util.HashMap;
@@ -29,29 +24,16 @@ public class AuthEntryPoint implements AuthenticationEntryPoint {
             AuthenticationException authException
     ) throws IOException {
 
-        HttpStatus status = HttpStatus.UNAUTHORIZED;
-        String message = "Authentication failed";
+        log.error("Unauthorized error: {}", authException.getMessage());
 
-        if (authException instanceof LockedException) {
-            status = HttpStatus.LOCKED;
-            message = "Your account has been locked. Please contact support.";
-        } else if (authException instanceof DisabledException) {
-            status = HttpStatus.FORBIDDEN;
-            message = "Your account has been disabled. Please contact support.";
-        } else if (authException instanceof CredentialsExpiredException) {
-            status = HttpStatus.FORBIDDEN;
-            message = "Your credentials have expired. Please reset your password.";
-        } else if (authException instanceof AccountExpiredException) {
-            status = HttpStatus.FORBIDDEN;
-            message = "Your account has expired. Please contact support.";
-        }
-
-        response.setStatus(status.value());
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
 
-        Map<String, String> responseBody = new HashMap<>();
-        responseBody.put("message", message);
+        final Map<String, Object> errorResponse = new HashMap<>();
+        errorResponse.put("status", HttpServletResponse.SC_UNAUTHORIZED);
+        errorResponse.put("error", "Unauthorized");
+        errorResponse.put("message", authException.getMessage());
 
-        response.getWriter().write(JsonUtils.toJson(responseBody));
+        response.getWriter().write(JsonUtils.toJson(errorResponse));
     }
 }
