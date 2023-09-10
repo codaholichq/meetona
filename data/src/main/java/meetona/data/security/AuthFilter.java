ab2dae5aaa74389244905264e0bdf5a8e759cd62
Emmy Steven <hello@emmysteven.com>
2023-09-10 19:57:15 +0100

    overhaul authentication filter

--- data/src/main/java/meetona/data/security/AuthFilter.java
+++ data/src/main/java/meetona/data/security/AuthFilter.java
@@ -4,8 +4,12 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
@@ -14,6 +18,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
 
 
+@Slf4j
 @Component
 public class AuthFilter extends OncePerRequestFilter {
 
@@ -30,11 +35,28 @@ public class AuthFilter extends OncePerRequestFilter {
             @NonNull FilterChain filterChain
     ) throws ServletException, IOException
     {
-        String token = tokenProvider.resolveToken(request);
-        if (StringUtils.hasText(token) && tokenProvider.validateToken(token)) {
+        String token = parseToken(request);
+        log.info("Extracting token from HttpServletRequest: {}", token);
+
+        if (token != null && tokenProvider.validateToken(token)) {
             Authentication auth = tokenProvider.getAuthentication(token);
-            SecurityContextHolder.getContext().setAuthentication(auth);
+
+            if (auth != null && !(auth instanceof AnonymousAuthenticationToken)) {
+                SecurityContext context = SecurityContextHolder.createEmptyContext();
+                context.setAuthentication(auth);
+                SecurityContextHolder.setContext(context);
+            }
         }
+
         filterChain.doFilter(request, response);
+    }
+
+    private String parseToken(HttpServletRequest request) {
+        String headerAuth = request.getHeader(HttpHeaders.AUTHORIZATION);
+
+        if (StringUtils.hasText(headerAuth) && headerAuth.startsWith("Bearer ")) {
+            return headerAuth.substring(7);
+        }
+        return null;
     }
 }
