78989d3461809f30961e34594c631bb387b3a908
Emmy Steven <hello@emmysteven.com>
2023-09-10 19:54:29 +0100

    check if username is null

--- data/src/main/java/meetona/data/repository/UserRepository.java
+++ data/src/main/java/meetona/data/repository/UserRepository.java
@@ -3,10 +3,11 @@ package meetona.data.repository;
 import meetona.core.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
-    User findByUsername(String username);
+    Optional<User> findByUsername(String username);
     Boolean existsByUsername(String username);
     Boolean existsByEmail(String email);
 }
