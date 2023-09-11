03bec6391faf675dc03f6bb667e1532961ab289c
Emmy Steven <hello@emmysteven.com>
2023-09-11 12:12:58 +0100

    use TomcatProtocolHandlerCustomizer

--- data/src/main/java/meetona/data/config/WebConfig.java
+++ data/src/main/java/meetona/data/config/WebConfig.java
@@ -1,16 +1,12 @@
 package meetona.data.config;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-//import org.springframework.boot.web.embedded.tomcat.TomcatProtocolHandlerCustomizer;
+import org.springframework.boot.web.embedded.tomcat.TomcatProtocolHandlerCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.task.AsyncTaskExecutor;
-import org.springframework.core.task.SimpleAsyncTaskExecutor;
-import org.springframework.lang.NonNull;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsConfigurationSource;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
-import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.time.Duration;
@@ -21,33 +17,16 @@ import java.util.concurrent.Executors;
 @ConditionalOnProperty(value = "thread-mode", havingValue = "virtual")
 public class WebConfig implements WebMvcConfigurer {
 
-    @Override
-    public void configureAsyncSupport(@NonNull AsyncSupportConfigurer configurer) {
-        configurer.setDefaultTimeout(-1);
-        configurer.setTaskExecutor(asyncTaskExecutor());
-    }
-
     @Bean
-    public AsyncTaskExecutor asyncTaskExecutor() {
-        SimpleAsyncTaskExecutor executor = new SimpleAsyncTaskExecutor() {
-            protected void doExecute(Runnable task) {
-                Thread.startVirtualThread(task);
-            }
-        };
-        executor.setThreadNamePrefix("async-");
-        return executor;
+    public TomcatProtocolHandlerCustomizer<?> protocolHandlerVirtualThreadExecutorCustomizer() {
+        return protocolHandler -> protocolHandler.setExecutor(Executors.newVirtualThreadPerTaskExecutor());
     }
-
-//    @Bean
-//    public TomcatProtocolHandlerCustomizer<?> protocolHandlerVirtualThreadExecutorCustomizer() {
-//        return protocolHandler -> protocolHandler.setExecutor(Executors.newVirtualThreadPerTaskExecutor());
-//    }
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration corsConfig = new CorsConfiguration();
         corsConfig.setAllowedOrigins(List.of("*"));
-        corsConfig.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+        corsConfig.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTION"));
         corsConfig.setAllowedHeaders(List.of("*"));
         corsConfig.setAllowedOrigins(List.of("http://localhost:4200"));
         corsConfig.setMaxAge(Duration.ofHours(1));
