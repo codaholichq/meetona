394391cf5486f7dc162132eaf836d4468adf17ad
Emmy Steven <hello@emmysteven.com>
2023-09-11 14:04:51 +0100

    fix: audit created_by and updated_by

--- data/src/main/java/meetona/data/config/AuditConfig.java
+++ data/src/main/java/meetona/data/config/AuditConfig.java
@@ -1,11 +1,9 @@
 package meetona.data.config;
 
-import meetona.core.entity.User;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.util.Optional;
@@ -17,14 +15,8 @@ public class AuditConfig {
     @Bean
     public AuditorAware<String> auditorAware() {
         return () -> {
-            Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-            if (auth != null && auth.isAuthenticated()) {
-                Object principal = auth.getPrincipal();
-                if (principal instanceof User) {
-                    return Optional.of(((User) principal).getUsername());
-                }
-            }
-            return Optional.empty();
+            String username = SecurityContextHolder.getContext().getAuthentication().getName();
+            return Optional.ofNullable(username);
         };
     }
 }
