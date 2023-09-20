a61ede4ca842c832caa200f85d3b22757d616a30
Emmy Steven <hello@emmysteven.com>
2023-09-20 21:49:24 +0100

    audit unknown user as SYSTEM

--- data/src/main/java/meetona/data/config/AuditConfig.java
+++ data/src/main/java/meetona/data/config/AuditConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.util.Optional;
@@ -15,7 +16,13 @@ public class AuditConfig {
     @Bean
     public AuditorAware<String> auditorAware() {
         return () -> {
-            String username = SecurityContextHolder.getContext().getAuthentication().getName();
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+            if (authentication == null || !authentication.isAuthenticated()) {
+                return Optional.of("SYSTEM");
+            }
+
+            var username = authentication.getName();
             return Optional.ofNullable(username);
         };
     }
