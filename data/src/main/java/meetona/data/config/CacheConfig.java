1ed7fa232f6b4ce8713e964250cac56814a40f9f
Emmy Steven <hello@emmysteven.com>
2023-09-20 03:15:23 +0100

    add configuration for caching

--- data/src/main/java/meetona/data/config/CacheConfig.java
+++ data/src/main/java/meetona/data/config/CacheConfig.java
@@ -1,17 +1,35 @@
 package meetona.data.config;
 
-import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext.SerializationPair;
+
+import java.time.Duration;
 
 @Configuration
 @EnableCaching
 public class CacheConfig {
 
     @Bean
-    public CacheManager cacheManager() {
-        return new ConcurrentMapCacheManager(); // or use another CacheManager implementation
+    public RedisCacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+        var cacheConfig = myDefaultCacheConfig(Duration.ofMinutes(10)).disableCachingNullValues();
+
+        return RedisCacheManager.builder(redisConnectionFactory)
+                .cacheDefaults(cacheConfig)
+                .withCacheConfiguration("units", myDefaultCacheConfig(Duration.ofMinutes(5)))
+                .withCacheConfiguration("unit", myDefaultCacheConfig(Duration.ofMinutes(1)))
+                .build();
+    }
+
+    private RedisCacheConfiguration myDefaultCacheConfig(Duration duration) {
+        return RedisCacheConfiguration
+                .defaultCacheConfig()
+                .entryTtl(duration)
+                .serializeValuesWith(SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()));
     }
 }
