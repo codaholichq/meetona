5ae67ac81b240f99922f26c481eb4436e18d8dbb
Emmy Steven <hello@emmysteven.com>
2023-09-21 12:28:22 +0100

    use JPQL in getting ADMIN roles

--- data/src/main/java/meetona/data/repository/UserRepository.java
+++ data/src/main/java/meetona/data/repository/UserRepository.java
@@ -1,13 +1,20 @@
 package meetona.data.repository;
 
 import meetona.core.entity.User;
+import meetona.core.enums.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.Collection;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByUsername(String username);
-    Boolean existsByUsername(String username);
-    Boolean existsByEmail(String email);
+    boolean existsByUsername(String username);
+    boolean existsByEmail(String email);
+
+    @Query("SELECT CASE WHEN COUNT(u) > 0 THEN true ELSE false END FROM User u WHERE u.roles = :roles")
+    boolean existsByRoles(@Param("roles") Collection<Role> roles);
 }
