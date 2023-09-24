1d655f6410cc0ed7aa9f3bd7978b58c9778f03f1
Emmy Steven <hello@emmysteven.com>
2023-09-24 23:29:03 +0100

    expose CRUD endpoints for department entity

---
+++ web/src/main/java/meetona/web/controller/DepartmentController.java
@@ -0,0 +1,57 @@
+package meetona.web.controller;
+
+import jakarta.validation.Valid;
+import meetona.core.interfaces.IDepartmentService;
+import meetona.core.payload.request.DepartmentRequest;
+import meetona.core.payload.response.ApiResponse;
+import meetona.core.payload.response.DepartmentDto;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/department")
+public class DepartmentController {
+
+    private final IDepartmentService departmentService;
+
+    public DepartmentController(IDepartmentService departmentService) {
+        this.departmentService = departmentService;
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<ApiResponse<List<DepartmentDto>>> getAll(@PageableDefault Pageable pageable) {
+        return ResponseEntity.ok(departmentService.getAll(pageable));
+    }
+
+    @GetMapping(value = "{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<ApiResponse<DepartmentDto>> getById(@PathVariable("id") UUID id) {
+        return ResponseEntity.ok(departmentService.getById(id));
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<ApiResponse<DepartmentDto>> add(@Valid @RequestBody DepartmentRequest request) {
+        return ResponseEntity.ok(departmentService.add(request));
+    }
+
+    @PutMapping(
+            value = "{id}",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<ApiResponse<DepartmentDto>> update(
+            @PathVariable("id") UUID id,
+            @RequestBody DepartmentRequest request
+    ) {
+        return ResponseEntity.ok(departmentService.update(id, request));
+    }
+
+    @DeleteMapping(value = "{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<ApiResponse<DepartmentDto>> delete(@PathVariable("id") UUID id){
+        return ResponseEntity.ok(departmentService.delete(id));
+    }
+}
