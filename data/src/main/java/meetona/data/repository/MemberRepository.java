19226919145b9f60c7cd7756dec29fd3b389ad67
Emmy Steven <hello@emmysteven.com>
2023-09-24 18:30:05 +0100

    add methods to member repository

--- data/src/main/java/meetona/data/repository/MemberRepository.java
+++ data/src/main/java/meetona/data/repository/MemberRepository.java
@@ -6,5 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
 
 public interface MemberRepository extends JpaRepository<Member, UUID> {
-
+    Boolean existsByPhoneNumber(String name);
+    Boolean existsByEmail(String email);
 }
