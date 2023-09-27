bb0672055d77164079122ced00ae5127d069a440
Emmy Steven <hello@emmysteven.com>
2023-09-27 12:24:41 +0100

    create role commandline runner

---
+++ data/src/main/java/meetona/data/runners/RoleRunner.java
@@ -0,0 +1,32 @@
+package meetona.data.runners;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import meetona.core.entity.Role;
+import meetona.core.enums.AppRole;
+import meetona.data.repository.RoleRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class RoleRunner implements CommandLineRunner {
+
+    private final RoleRepository roleRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        boolean existsRoles = roleRepository.findAll().isEmpty();
+
+        if (existsRoles) {
+            for (AppRole appRole : AppRole.values()) {
+                Role role = new Role();
+                role.setName(appRole);
+                role.setDescription("");
+                roleRepository.save(role);
+            }
+            log.info("Roles added successfully");
+        }
+    }
+}
