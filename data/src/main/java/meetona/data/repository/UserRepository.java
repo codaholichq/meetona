e6627c3bfaa43282147c8f10f5c31b3700490169
Emmy Steven <hello@emmysteven.com>
2023-09-28 01:37:05 +0100

    add method to check if roles exist

--- data/src/main/java/meetona/data/repository/UserRepository.java
+++ data/src/main/java/meetona/data/repository/UserRepository.java
@@ -1,12 +1,9 @@
 package meetona.data.repository;
 
+import meetona.core.entity.Role;
 import meetona.core.entity.User;
-import meetona.core.enums.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
-import java.util.Collection;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -14,7 +11,5 @@ public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByUsername(String username);
     boolean existsByUsername(String username);
     boolean existsByEmail(String email);
-
-    @Query("SELECT CASE WHEN COUNT(u) > 0 THEN true ELSE false END FROM User u WHERE u.roles = :roles")
-    boolean existsByRoles(@Param("roles") Collection<Role> roles);
+    boolean existsByRoles(Role role);
 }
