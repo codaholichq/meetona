8b65ec7c9ab52f301fb81893ff408d5707f8cfc5
Emmy Steven <hello@emmysteven.com>
2023-10-12 16:05:07 +0100

    add index for folder

--- client/src/plugins/index.js
+++ client/src/plugins/index.js
@@ -1,1 +1,2 @@
-export * from './font.awesome';
+export { default as FontAwesomeIcon } from './font.awesome';
+export { default as validation } from './validation'
--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -1,53 +1,49 @@
 <template>
-  <div class="col-md-12">
-    <div class="card card-container">
-      <img
-        id="profile-img"
-        src="//ssl.gstatic.com/accounts/ui/avatar_2x.png"
-        class="profile-img-card"
-      />
-      <Form @submit="handleLogin" :validation-schema="schema">
-        <div class="form-group">
-          <label for="username">Username</label>
-          <Field name="username" type="text" class="form-control" />
-          <ErrorMessage name="username" class="error-feedback" />
-        </div>
-        <div class="form-group">
-          <label for="password">Password</label>
-          <Field name="password" type="password" class="form-control" />
-          <ErrorMessage name="password" class="error-feedback" />
-        </div>
-
-        <div class="form-group">
-          <button class="btn btn-primary btn-block" :disabled="loading">
-            <span v-show="loading" class="spinner-border spinner-border-sm"></span>
-            <span>Login</span>
-          </button>
-        </div>
-
-        <div class="form-group">
-          <div v-if="message" class="alert alert-danger" role="alert">
-            {{ message }}
-          </div>
-        </div>
-      </Form>
+  <div>
+    <div class="alert alert-info">
+      Username: test<br />
+      Password: test
     </div>
+    <h2>Login</h2>
+    <vee-form @submit="login" :validation-schema="schema" v-slot="{ errors, isSubmitting }">
+      <div class="form-group">
+        <label>Username</label>
+        <vee-field
+          name="username"
+          type="text"
+          class="form-control"
+          :class="{ 'is-invalid': errors.username }"
+        />
+        <div class="invalid-feedback">{{ errors.username }}</div>
+      </div>
+      <div class="form-group">
+        <label>Password</label>
+        <vee-field
+          name="password"
+          type="password"
+          class="form-control"
+          :class="{ 'is-invalid': errors.password }"
+        />
+        <div class="invalid-feedback">{{ errors.password }}</div>
+      </div>
+      <div class="form-group">
+        <button class="btn btn-primary" :disabled="isSubmitting">
+          <span v-show="isSubmitting" class="spinner-border spinner-border-sm mr-1"></span>
+          Login
+        </button>
+      </div>
+      <div v-if="errors.apiError" class="alert alert-danger mt-3 mb-0">{{ errors.apiError }}</div>
+    </vee-form>
   </div>
 </template>
 
 <script>
-import { Form, Field, ErrorMessage } from 'vee-validate';
 import * as yup from 'yup';
+import { useAuthStore } from '@/stores';
 
 export default {
   // eslint-disable-next-line vue/multi-word-component-names
   name: 'Login',
-  components: {
-    // eslint-disable-next-line vue/no-reserved-component-names
-    Form,
-    Field,
-    ErrorMessage
-  },
   data() {
     const schema = yup.object().shape({
       username: yup.string().required('Username is required!'),
@@ -65,18 +61,19 @@ export default {
   //     return this.$store.state.auth.status.loggedIn;
   //   }
   // },
-  // created() {
-  //   if (this.loggedIn) {
-  //     this.$router.push('/profile');
-  //   }
-  // },
+  created() {
+    if (this.loggedIn) {
+      this.$router.push('/admin');
+    }
+  },
   methods: {
-    handleLogin(user) {
+    login(username, password) {
       this.loading = true;
+      const authStore = useAuthStore();
 
-      this.$store.dispatch('auth/login', user).then(
+      authStore.login(username, password).then(
         () => {
-          this.$router.push('/profile');
+          this.$router.push('/admin');
         },
         (error) => {
           this.loading = false;
@@ -89,6 +86,17 @@ export default {
     }
   }
 };
+
+// export default {
+// eslint-disable-next-line vue/multi-word-component-names
+// name: 'Login'
+// function login(values, { setErrors }) {
+//   const authStore = useAuthStore();
+//   const { username, password } = values;
+
+//   return authStore.login(username, password).catch((error) => setErrors({ apiError: error }));
+// }
+// };
 </script>
 
 <style scoped>
