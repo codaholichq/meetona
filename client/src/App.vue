5b2cd30cbc71f6fbb349e9ad0a5cb38ae634e8ec
Emmy Steven <hello@emmysteven.com>
2023-10-13 23:49:05 +0100

    remove default styling

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,46 +1,22 @@
 <script setup>
 import { RouterView } from 'vue-router';
 import AppHeader from '@/components/Header.vue';
+import AppFooter from "@/components/Footer.vue";
 </script>
 
 <template>
   <AppHeader />
-  <RouterView />
+  <div class="col-md-6 offset-md-3 mt-5 mb-5">
+    <RouterView />
+  </div>
+  <AppFooter />
 </template>
 
 <style>
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
+.mt-5 {
+  padding-top: 6rem;
 }
-body {
-  color: #b4b8bd;
-  font-family: 'montserrat', sans-serif;
-}
-header {
-  width: 100vw;
-  background-color: rgb(88, 82, 82);
-  padding: 25px;
-}
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #202427;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+.mb-5 {
+  padding-bottom: 2rem;
 }
 </style>
