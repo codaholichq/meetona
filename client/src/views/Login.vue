93bf7c72b8c8acaa79774407b32b746a4f897309
Emmy Steven <74903385+emmysteven@users.noreply.github.com>
2023-10-14 00:10:06 +0100

    properly center login form (#16)
    * remove default styling
    
    * add logo to header
    
    * remove style for login view
    
    * add utility to handle api calls
    
    * add footer component
    
    * fix: pass form data properly
    
    * add index to folder

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -1,39 +1,48 @@
 <template>
-  <div>
-    <div class="alert alert-info">
-      Username: test<br />
-      Password: test
+  <div class="col-md-6 offset-md-3 mt-5">
+    <div class="card mt-2">
+      <h4 class="card-header">Login</h4>
+      <div class="card-body">
+
+        <vee-form @submit="login" :validation-schema="schema" v-slot="{ errors, isSubmitting }">
+          <div class="form-group">
+            <label for="username">Username</label>
+            <vee-field
+              id="username"
+              name="username"
+              type="text"
+              autocomplete="on"
+              class="form-control"
+              :class="{ 'is-invalid': errors.username }"
+            />
+            <div class="invalid-feedback">{{ errors.username }}</div>
+          </div>
+
+          <div class="form-group mt-3">
+            <label for="password">Password</label>
+            <vee-field
+              id="password"
+              name="password"
+              type="password"
+              autocomplete="on"
+              class="form-control"
+              :class="{ 'is-invalid': errors.password }"
+            />
+            <div class="invalid-feedback">{{ errors.password }}</div>
+          </div>
+
+          <div class="form-group">
+            <button class="btn btn-primary mt-4" :disabled="isSubmitting">
+              <span v-show="isSubmitting" class="spinner-border spinner-border-sm mr-1"></span>
+              Login
+            </button>
+          </div>
+
+          <div v-if="errors.apiError" class="alert alert-danger mt-3 mb-0">{{ errors.apiError }}</div>
+        </vee-form>
+
+      </div>
     </div>
-    <h2>Login</h2>
-    <vee-form @submit="login" :validation-schema="schema" v-slot="{ errors, isSubmitting }">
-      <div class="form-group">
-        <label>Username</label>
-        <vee-field
-          name="username"
-          type="text"
-          class="form-control"
-          :class="{ 'is-invalid': errors.username }"
-        />
-        <div class="invalid-feedback">{{ errors.username }}</div>
-      </div>
-      <div class="form-group">
-        <label>Password</label>
-        <vee-field
-          name="password"
-          type="password"
-          class="form-control"
-          :class="{ 'is-invalid': errors.password }"
-        />
-        <div class="invalid-feedback">{{ errors.password }}</div>
-      </div>
-      <div class="form-group">
-        <button class="btn btn-primary" :disabled="isSubmitting">
-          <span v-show="isSubmitting" class="spinner-border spinner-border-sm mr-1"></span>
-          Login
-        </button>
-      </div>
-      <div v-if="errors.apiError" class="alert alert-danger mt-3 mb-0">{{ errors.apiError }}</div>
-    </vee-form>
   </div>
 </template>
 
@@ -56,6 +65,7 @@ export default {
       schema
     };
   },
+
   // computed: {
   //   loggedIn() {
   //     return this.$store.state.auth.status.loggedIn;
@@ -66,6 +76,7 @@ export default {
       this.$router.push('/admin');
     }
   },
+
   methods: {
     login(username, password) {
       this.loading = true;
@@ -85,55 +96,9 @@ export default {
       );
     }
   }
-};
-
-// export default {
-// eslint-disable-next-line vue/multi-word-component-names
-// name: 'Login'
-// function login(values, { setErrors }) {
-//   const authStore = useAuthStore();
-//   const { username, password } = values;
-
-//   return authStore.login(username, password).catch((error) => setErrors({ apiError: error }));
-// }
-// };
+}
 </script>
 
 <style scoped>
-label {
-  display: block;
-  margin-top: 10px;
-}
 
-.card-container.card {
-  max-width: 350px !important;
-  padding: 40px 40px;
-}
-
-.card {
-  background-color: #f7f7f7;
-  padding: 20px 25px 30px;
-  margin: 0 auto 25px;
-  margin-top: 50px;
-  -moz-border-radius: 2px;
-  -webkit-border-radius: 2px;
-  border-radius: 2px;
-  -moz-box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
-  -webkit-box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
-  box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
-}
-
-.profile-img-card {
-  width: 96px;
-  height: 96px;
-  margin: 0 auto 10px;
-  display: block;
-  -moz-border-radius: 50%;
-  -webkit-border-radius: 50%;
-  border-radius: 50%;
-}
-
-.error-feedback {
-  color: red;
-}
 </style>
