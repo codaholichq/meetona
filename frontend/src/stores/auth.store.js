2aa25eef01b1764d7d0efc55e8d3ba2883a41d44
Emmy Steven <hello@emmysteven.com>
2023-10-17 22:53:16 +0100

    make logout action async

--- frontend/src/stores/auth.store.js
+++ frontend/src/stores/auth.store.js
@@ -1,5 +1,4 @@
 import { defineStore } from 'pinia';
-
 import { apiWrapper } from '@/helpers';
 import router from '@/router'
 
@@ -7,27 +6,25 @@ const API_URL = import.meta.env.VITE_API_URL;
 
 export const useAuthStore = defineStore({
   id: 'auth',
+
   state: () => ({
-    // initialize state from local storage to enable user to stay logged in
     user: JSON.parse(localStorage.getItem('token')),
     returnUrl: null
   }),
+
   actions: {
     async login(data) {
       const user = await apiWrapper.post(`${API_URL}/auth`, data);
 
-      // update pinia state
       this.user = user;
-
-      // store user details and jwt in local storage to keep user logged in between page refreshes
       localStorage.setItem('token', JSON.stringify(user));
 
-      // redirect to previous url or default to home page
       router.push(this.returnUrl || '/');
     },
-    logout() {
+
+    async logout() {
       this.user = null;
-      localStorage.removeItem('user');
+      localStorage.removeItem('token');
       router.push('/login');
     }
   }
