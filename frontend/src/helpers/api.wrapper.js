0c868a2f633917ca00a79407d855c9a407b081fa
Emmy Steven <hello@emmysteven.com>
2023-10-28 22:54:14 +0100

    replace fetch with axios

--- frontend/src/helpers/api.wrapper.js
+++ frontend/src/helpers/api.wrapper.js
@@ -1,49 +1,35 @@
+import axios from 'axios';
 import { useAuthStore } from '@/stores';
 
-export const apiWrapper = {
-    get: request('GET'),
-    post: request('POST'),
-    put: request('PUT'),
-    delete: request('DELETE')
-};
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL
+});
+
+api.interceptors.request.use((config) => {
+  const { user } = useAuthStore();
+  const token = user?.data.accessToken;
+
+  if (token) {
+    config.headers['Authorization'] = `Bearer ${token}`;
+  }
+  return config;
+});
 
 function request(method) {
-    return (url, body) => {
-        const requestOptions = {
-            method,
-            headers: authHeader(url)
-        };
-        if (body) {
-            requestOptions.headers['Content-Type'] = 'application/json';
-            requestOptions.body = JSON.stringify(body);
-        }
-        return fetch(url, requestOptions).then(handleResponse);
+  return (url, body) => {
+    const requestOptions = { method }
+
+    if (body) {
+      requestOptions.headers = { 'Content-Type': 'application/json' };
+      requestOptions.data = body;
     }
+    return api(url, requestOptions).then(response => response.data);
+  }
 }
 
-/** Helper Functions **/
-function authHeader(url) {
-    const { user } = useAuthStore();
-    const token = user?.data.accessToken;
-    const isApiUrl = url.startsWith(import.meta.env.VITE_API_URL);
-    if (!!token && isApiUrl) {
-        return { Authorization: `Bearer ${token}` }
-    }
-    return {};
-}
-
-function handleResponse(response) {
-    return response.text().then(text => {
-        const data = text && JSON.parse(text);
-
-        if (!response.ok) {
-            const { user, logout } = useAuthStore();
-            if ([401, 403].includes(response.status) && user) {
-                logout();
-            }
-            const error = (data && data.message) || response.statusText;
-            return Promise.reject(error);
-        }
-        return data;
-    });
-}
+export const apiWrapper = {
+  get: request('GET'),
+  post: request('POST'),
+  put: request('PUT'),
+  delete: request('DELETE')
+};
