e0308d9aad5d9fcf42af754a383c356af3fcf58d
Emmy Steven <hello@emmysteven.com>
2023-11-17 13:42:43 +0100

    trailing slashes should not return 404
    fix #24

---
+++ backend/src/main/java/meetona/shared/filters/TrailingSlashFilter.java
@@ -0,0 +1,48 @@
+package meetona.shared.filters;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletRequestWrapper;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class TrailingSlashFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        String path = httpRequest.getRequestURI();
+        if (path.endsWith("/")) {
+            String newPath = path.substring(0, path.length() - 1);
+            HttpServletRequest newRequest = new HttpRequestWrapper(httpRequest, newPath);
+            chain.doFilter(newRequest, response);
+        }
+        chain.doFilter(request, response);
+    }
+
+    private static class HttpRequestWrapper extends HttpServletRequestWrapper {
+        private final String newPath;
+        public HttpRequestWrapper(HttpServletRequest request, String newPath) {
+            super(request);
+            this.newPath = newPath;
+        }
+        @Override
+        public String getRequestURI() {
+            return newPath;
+        }
+        @Override
+        public StringBuffer getRequestURL() {
+            StringBuffer url = new StringBuffer();
+            url.append(getScheme())
+                    .append("://")
+                    .append(getServerName())
+                    .append(":")
+                    .append(getServerPort())
+                    .append(newPath);
+            return url;
+        }
+    }
+}
