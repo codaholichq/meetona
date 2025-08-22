11c175d8be17a6fdb1866e633327f007f4a91ce1
Emmy Steven <hello@emmysteven.com>
2025-08-22 14:26:50 +0100

    feat(auth): add /api/cell/** to ADMIN routes
    fix #32

--- backend/src/main/java/meetona/user/security/SecurityConfig.java
+++ backend/src/main/java/meetona/user/security/SecurityConfig.java
@@ -32,7 +32,7 @@ import java.util.List;
 public class SecurityConfig {
 
     protected static final String[] ADMIN = {
-            "/api/unit/**",
+            "/api/cell/**",
             "/api/user/**",
             "/api/member/**",
             "/actuator/**"
