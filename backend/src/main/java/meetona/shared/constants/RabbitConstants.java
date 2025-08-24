a7e5b9067aad2520fdd19fca70a9ac0b0f8f72ba
Emmy Steven <hello@emmysteven.com>
2025-08-24 03:02:59 +0100

    feat(rabbit): add UNIT_ROUTING_KEY constant for cell messages

--- backend/src/main/java/meetona/shared/constants/RabbitConstants.java
+++ backend/src/main/java/meetona/shared/constants/RabbitConstants.java
@@ -10,7 +10,7 @@ public class RabbitConstants {
     public static final String MEETING_QUEUE = "q.meetona.meeting";
 
     public static final String USER_ROUTING_KEY = "k.meetona.user";
-    public static final String UNIT_ROUTING_KEY = "k.meetona.unit";
+    public static final String UNIT_ROUTING_KEY = "k.meetona.cell";
 
     public static final String MEMBER_ROUTING_KEY = "k.meetona.member";
     public static final String MEETING_ROUTING_KEY = "k.meetona.meeting";
